c2b89e0d5ba6a27f28d9d01d69352b7289deab51
rafal <raf.drozdowski@gmail.com>
2020-03-09 19:16:01 +0000

    get movies list from file, delete manudall added json movie list

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,40 @@
  * @constructor Movies
  *  
  */
-const Movies = function () {
+
+ const Movies = function () {
 };
 
 /**
+ * @method fetchAll fetch all movies
  * 
+ * @returns {obj} movies list
  */
 Movies.prototype.fetchAll = function () {
 
+  //change context
+  const _self = this;
   const config = {
-    API_KEY: "574c3d28a19ee50fbd1077e77c56fd67",
-    url: "https://api.themoviedb.org/4/list/1?page=1&api_key="
+    url: "https://api.themoviedb.org/4/list/1?page=1&api_key=574c3d28a19ee50fbd1077e77c56fd67"
   };
 
-  fetch(config.url).
-    then(e => e.json()).
-    then(e => console.log(e))
+   let data = fetch(config.url).then(function(response){
+      return response.json()
+    }).then(function(data){
+      _self.showMovies(data.results);
+      debugger;
+    });
+   
+  return data;
 
 };
 
-const movies = new Movies();
+Movies.prototype.showMovies = function (arg) {
+  for( var i = 0; i<= arg.length; i++){
+    console.log(arg[i]);
+  }
 
+}
+
+const movies = new Movies();
 movies.fetchAll();
